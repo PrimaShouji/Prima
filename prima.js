22e3458cbf1297de1382f73700b3c390480c2edd
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-10-14 17:39:18 -0700

    remove tab characters

--- prima.js
+++ prima.js
@@ -10,19 +10,19 @@ const db = MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: 
 const logger = winston.createLogger({
 	transports: [
 		new winston.transports.Console(),
-        new DailyRotateFile({
-            datePattern: "YYYY-MM-DD-HH",
-            filename: "logs/%DATE%.log",
-            maxSize: "20m"
-        }),
-        new winston.transports.File({
-            filename: "logs/error.log",
-            level: "error"
-        })
+		new DailyRotateFile({
+			datePattern: "YYYY-MM-DD-HH",
+			filename: "logs/%DATE%.log",
+			maxSize: "20m"
+		}),
+		new winston.transports.File({
+			filename: "logs/error.log",
+			level: "error"
+		})
 	],
 	format: winston.format.combine(
 		winston.format.timestamp({
-		  format: 'HH:mm:ss'
+			format: 'HH:mm:ss'
 		}),
 		winston.format.printf((log) => `[${log.timestamp}][${log.level.toUpperCase()}] - ${log.message}`),
 	),
