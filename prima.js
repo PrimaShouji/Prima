347cb7ae046b2bcc26d2a1770d6053727d45e2b2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-10-14 17:41:30 -0700

    remove tab characters

--- prima.js
+++ prima.js
@@ -8,24 +8,24 @@ const Domain          = require("./lib/subsystem/Domain");
 // Resource setup
 const db = MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
 const logger = winston.createLogger({
-	transports: [
-		new winston.transports.Console(),
-		new DailyRotateFile({
-			datePattern: "YYYY-MM-DD-HH",
-			filename: "logs/%DATE%.log",
-			maxSize: "20m"
-		}),
-		new winston.transports.File({
-			filename: "logs/error.log",
-			level: "error"
-		})
-	],
-	format: winston.format.combine(
-		winston.format.timestamp({
-			format: 'HH:mm:ss'
-		}),
-		winston.format.printf((log) => `[${log.timestamp}][${log.level.toUpperCase()}] - ${log.message}`),
-	),
+    transports: [
+        new winston.transports.Console(),
+        new DailyRotateFile({
+            datePattern: "YYYY-MM-DD-HH",
+            filename: "logs/%DATE%.log",
+            maxSize: "20m"
+        ),
+        new winston.transports.File({
+            filename: "logs/error.log",
+            level: "error"
+        })
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'HH:mm:ss'
+        }),
+        winston.format.printf((log) => `[${log.timestamp}][${log.level.toUpperCase()}] - ${log.message}`),
+    ),
 });
 
 // Domain setup
