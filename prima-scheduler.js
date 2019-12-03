71c8866cfa0a0912ac625d84b14b6b191b1a1398
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-12-03 11:02:50 -0800

    Minimize code redundancy

--- prima-scheduler.js
+++ prima-scheduler.js
@@ -1,11 +1,9 @@
 // Dependencies
 const Discord         = require("discord.js");
-const fs              = require("fs");
-const { MongoClient } = require("mongodb");
-const winston         = require("winston");
-const DailyRotateFile = require("winston-daily-rotate-file")
 
+const createLogger    = require("./lib/util/createLogger");
 const ensureConfig    = require("./lib/util/ensureConfig");
+const loadEvents      = require("./lib/util/loadEvents");
 
 const GoogleSheets    = require("./lib/subsystem/GoogleSheets");
 
@@ -16,46 +14,13 @@ const { token } = ensureConfig("./config.json");
 const client = new Discord.Client();
 
 // Resource setup
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console(),
-        new DailyRotateFile({
-            datePattern: "YYYY-MM-DD-HH",
-            filename: "logs/%DATE%.log",
-            maxSize: "20m"
-        }),
-        new winston.transports.File({
-            filename: "logs/error.log",
-            level: "error"
-        })
-    ],
-    format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'HH:mm:ss'
-        }),
-        winston.format.printf((log) => `[${log.timestamp}][${log.level.toUpperCase()}] - ${log.message}`),
-    ),
-});
-
 client.domain = "scheduler";
+client.logger = createLogger();
 
 // Client events
-const eventFiles = fs.readdirSync("./lib/events/").filter((fileName) => fileName.endsWith(".js"));
-for (let i = 0; i < eventFiles.length; i++) {
-    const e = require("./lib/events/" + eventFiles[i]);
-    const eventName = eventFiles[i].substr(0, eventFiles[i].indexOf("."));
-    if (e.domain && e.domain !== client.domain) continue;
+loadEvents(client, client.logger);
 
-    if (client.listeners(eventName)) {
-        if (e.domain) client.removeAllListeners(eventName);
-    }
-
-    client.on(
-        eventName,
-        e.bind(null, client, logger)
-    );
-}
-
+// Login
 client.login(token)
 .then(() => {
     client.sheets = new GoogleSheets();
