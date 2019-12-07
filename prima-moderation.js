69eea628458e71983558155e91e4b6206e29c5b0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-12-06 16:51:15 -0800

    tempban cronjob

---
+++ prima-moderation.js
@@ -0,0 +1,33 @@
+// Dependencies
+const Discord         = require("discord.js");
+const { MongoClient } = require("mongodb");
+
+const createLogger    = require("./lib/util/createLogger");
+const ensureConfig    = require("./lib/util/ensureConfig");
+const loadCronJobs    = require("./lib/util/loadCronJobs");
+const loadEvents      = require("./lib/util/loadEvents");
+
+// Config load/creation
+const { token } = ensureConfig("./config.json");
+
+// Bot client initialization
+const client = new Discord.Client();
+
+// Resource setup
+client.domain = "moderation";
+client.logger = createLogger();
+
+// Put this in a subsystem later
+const { MongoClient } = require("mongodb");
+
+const dbURL = "mongodb://localhost:27017/";
+client.dbName = "prima_db";
+
+client.db = MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Client events
+loadCronJobs(client);
+loadEvents(client);
+
+// Login
+client.login(token);
