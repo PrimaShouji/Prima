f822c8d82a031ef41a54a05d2a10d8cae49c4124
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-12-14 15:06:58 -0800

    Restart core, remove old scheduler

--- Prima_Schedule/commands/ping.js
+++
@@ -1,8 +0,0 @@
-const pjson = require('../package.json');
-
-module.exports = {
-	name: 'ping',
-	execute(client, message, logger, args) {
-		message.channel.send(`\`${pjson.name} online, heartbeat ${client.ping}ms\``);
-	}
-}
--- Prima_Schedule/events/error.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (client, logger, message) => {
-	logger.error(message);
-}
