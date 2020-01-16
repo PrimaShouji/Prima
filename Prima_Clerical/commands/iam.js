104a714133cb712f0cbacba377bdaf3df6381236
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-01-15 16:11:01 -0800

    Support Coeurl typos

--- Prima_Clerical/commands/iam.js
+++ Prima_Clerical/commands/iam.js
@@ -27,7 +27,7 @@ module.exports = {
 		const MIN_LEVEL = 60;
 
 		// Sanitation for the initial query.
-		var world = args[0].replace(/[^a-zA-Z]/g, "");
+		var world = args[0].replace(/[^a-zA-Z]/g, "").replace(/(?:courel|couerl)/g, "Coeurl");
 		var name = (args[1] + " " + args[2]).replace(/[<>]/g, "").replace(/’/g, "'"));
 
 		xiv.character.search(name, { server: world }).then(async (res, err) => {
